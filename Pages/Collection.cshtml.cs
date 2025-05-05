9ae8e181f5874b788a5e6d6fae76bba358b4c73e
QuocDat <vnqd0301@gmail.com>
2025-05-05 08:39:34 +0700

    feat: create Faq layout page

---
+++ Pages/Collection.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cheznana.Pages
+{
+    public class CollectionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,51 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Envent mouse scroll
+document.addEventListener("DOMContentLoaded", function () {
+    const left = document.getElementById("collection-left");
+    const right = document.getElementById("collection-right");
 
-// Write your JavaScript code.
+    // Sự kiện wheel cho left
+    left.addEventListener("wheel", function (e) {
+        if (left.scrollTop == 0 && right.scrollTop > 0) {
+            e.preventDefault();
+            const delta = e.deltaY;
+            if (delta < 0) {
+                right.scrollTop += delta;
+            }
+            else {
+                left.scrollTop += delta;
+            }
+        }
+        else if (left.scrollTop == left.scrollHeight - left.clientHeight && right.scrollTop < right.scrollHeight - right.clientHeight) {
+            e.preventDefault();
+            const delta = e.deltaY;
+            if (delta < 0) {
+                left.scrollTop += delta;
+            } else {
+                right.scrollTop += delta;
+            }
+        }
+    });
+
+    // Sự kiện wheel cho right
+    right.addEventListener("wheel", function (e) {
+        if (right.scrollTop == 0 && left.scrollTop > 0) {
+            e.preventDefault();
+            const delta = e.deltaY;
+            if (delta < 0) {
+                left.scrollTop += delta;
+            }
+            else {
+                right.scrollTop += delta;
+            }
+        }
+        else if (right.scrollTop == right.scrollHeight - right.clientHeight && left.scrollTop < left.scrollHeight - left.clientHeight) {
+            e.preventDefault();
+            const delta = e.deltaY;
+            if (delta < 0) {
+                right.scrollTop += delta;
+            } else {
+                left.scrollTop += delta;
+            }
+        }
+    });
+});
